0150e3c2a047a675a684cd913f59df13f0fe44d7
Pierce Darragh <pierce.darragh@gmail.com>
2020-10-23 15:38:00 -0600

    Add simple rendering for ASTs

--- Sources/PwZ/AST.swift
+++ Sources/PwZ/AST.swift
@@ -48,13 +48,139 @@ fileprivate func prepend<T>(elementsOf xs: [T], toEach yss: inout [[T]]) {
     }
 }
 
+fileprivate func render(ast: AST) -> String {
+    var texts = [String]()
+
+    func render(ast: AST, withIndentation indentation: Int) {
+        let indentationText = String.init(repeating: " ", count: indentation)
+        switch (ast) {
+        case let .Tok(token):
+            texts.append(indentationText + "TOKEN: " + token.symbol)
+        case let .Seq(symbol, asts):
+            if asts.isEmpty {
+                texts.append(indentationText + symbol)
+            } else {
+                texts.append(indentationText + "(" + symbol)
+                asts.forEach { render(ast: $0, withIndentation: indentation + 3) }
+                texts.append(texts.removeLast() + ")")
+            }
+        }
+    }
+
+    render(ast: ast, withIndentation: 0)
+
+    return texts.joined(separator: "\n")
+}
+
 /// Encodes the AST resulting from processing a parsed grammar expression.
-public indirect enum AST {
+public indirect enum AST: CustomStringConvertible {
     /// An atomic token production.
     case Tok(token: Token)
     /// A named sequence of sub-trees.
     case Seq(symbol: Symbol, children: [AST])
+
+    public var description: String {
+        return render(ast: self)
+    }
 }
+
+/*
+
+e ::= A
+    | e e
+
+import PwZ
+let grammar: Grammar = [ Grammar.startSymbol: "e"
+                       , "e":
+                           [ "A"
+                           , "e" ++ "e"
+                           ]
+                       ]
+let tokens: [Token] = grammar.tokenizeSymbols(["A", "A", "A", "A"])
+let exps = grammar.parse(withInputTokens: tokens)
+let asts = exps.flatMap(produceASTsFromExpression)
+asts.forEach { print($0) }
+
+
+OCaml:
+#5
+  (ee
+     (ee
+        A
+        (ee
+           A
+           A))
+     A)
+#3
+  (ee
+     (ee
+        (ee
+           A
+           A)
+        A)
+     A)
+#1
+  (ee
+     (ee
+        A
+        A)
+     (ee
+        A
+        A))
+#2
+  (ee
+     A
+     (ee
+        A
+        (ee
+           A
+           A)))
+#4
+  (ee
+     A
+     (ee
+        (ee
+           A
+           A)
+        A))
+
+
+
+Swift:
+(ee
+   (ee
+      (ee
+         A
+         A)
+      A)
+   (ee
+      A
+      (ee
+         A
+         A))
+   A)
+(ee
+   (ee
+      A
+      A)
+   (ee
+      A
+      A))
+(ee
+   A
+   (ee
+      (ee
+         A
+         A)
+      A)
+   (ee
+      A
+      (ee
+         A
+         A)))
+
+
+ */
 
 /**
  Returns a list of ASTs that can be represented by the given expression. If the
